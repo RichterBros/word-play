62ccc876a8e530250fc81871d8c42352cda0c77a
Erich Richter <erich.richter@gmail.com>
2020-06-08 14:53:38 -0700

    add site functionality > > Co-Authored-by: Kate Skorija <Kate Skorija kateskorija@gmail.com> Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@ $(document).ready(function(){
   $("#form-one").submit(function(event) {
     event.preventDefault();
 
-
     let inputsArr = ($("input#input").val()).split(" ");
-    
+    console.log(inputsArr)
     let newArray = [];
     inputsArr.forEach(function(input){
       if (input.length >= 3){
           newArray.push(input);
       } 
+    reversedArray = newArray.reverse().join(" ");
     });
-    console.log(newArray);
+    
+    $("#output").text(reversedArray);
+    console.log(reversedArray);
   });
 });
