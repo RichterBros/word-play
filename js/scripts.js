3f6e543968bbd94e8f24f5bcb6e2a755d3600852
Erich Richter <erich.richter@gmail.com>
2020-06-08 14:36:55 -0700

    add js, html functionality > > Co-Authored-by: Kate Skorija <Kate Skorija kateskorija@gmail.com> Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+  $("#form-one").submit(function(event) {
+    event.preventDefault();
 
+
+    let inputsArr = ($("input#input").val()).split(" ");
+    console.log(inputsArr);
+    let newArray = [];
+    inputsArr.forEach(function(input){
+      if (input.length >= 3){
+          newArray.push(input);
+      } 
+
+    });
+  });
 });
